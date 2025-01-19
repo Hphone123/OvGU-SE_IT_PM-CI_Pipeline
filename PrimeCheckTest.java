c940753be4c4f6354e2b6422998a0f43085db46f
Hannes Krüger <kruegerh@kd-elektroniksysteme.de>
2025-01-19 14:24:07 +0100

    Finish UnitTests

--- PrimeCheckTest.java
+++ PrimeCheckTest.java
@@ -22,14 +22,16 @@ public class PrimeCheckTest {
   // --------------------------------------------------------------- //
   @Test
   public void testPrimes() {
-    /* ToDo: insert a test here */
-    assertEquals("this will fail", true, false);
+    assertTrue("Test 0: ", PrimeCheck.isPrime(97));
+    assertTrue("Test 1: ", PrimeCheck.isPrime(1427));
+    assertTrue("Test 2: ", PrimeCheck.isPrime(5569));
   }
 
   // --------------------------------------------------------------- //
   @Test
   public void testNonPrimes() {
-    /* ToDo: insert a test here */
-    assertEquals("this will fail", true, false);
+    assertFalse("Test 0: ", PrimeCheck.isPrime(5570));
+    assertFalse("Test 1: ", PrimeCheck.isPrime(4));
+    assertFalse("Test 2: ", PrimeCheck.isPrime(4444));
   }
 }
